05a1f1ed7e5547ff2eeaa7e69d8fdab11bdaee34
hugo <hugomendonca9@gmail.com>
2022-01-12 16:25:46 -0300

    configuring eslint and prettier

--- src/modules/users/repositories/IUserRepository.ts
+++ src/modules/users/repositories/IUserRepository.ts
@@ -2,8 +2,8 @@ import ICreateUserDTO from "../dtos/ICreateUserDTO";
 import User from "../infra/typeorm/entities/User";
 
 export default interface IUserRepository {
-    findById(id: string): Promise<User | undefined>;
-    findByEmail(email: string): Promise<User | undefined>;
-    create(data: ICreateUserDTO): Promise<User>;
-    save(user: User): Promise<User>;
+  findById(id: string): Promise<User | undefined>;
+  findByEmail(email: string): Promise<User | undefined>;
+  create(data: ICreateUserDTO): Promise<User>;
+  save(user: User): Promise<User>;
 }
--- src/modules/users/services/SessionUserService.ts
+++ src/modules/users/services/SessionUserService.ts
@@ -1,50 +1,52 @@
-import authConfig from "@config/auth";
-import AppError from "@shared/errors/appError";
-import User from "../infra/typeorm/entities/User";
-import IHashProvider from "../providers/hashProvider/models/IHashProvider";
-import IUserRepository from "../repositories/IUserRepository";
-
+import authConfig from '@config/auth';
+import AppError from '@shared/errors/appError';
 import { sign } from 'jsonwebtoken';
+import User from '../infra/typeorm/entities/User';
+import IHashProvider from '../providers/hashProvider/models/IHashProvider';
+import IUserRepository from '../repositories/IUserRepository';
 
 interface IRequest {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 interface IResponse {
-    user: User,
-    token: string,
+  user: User;
+  token: string;
 }
 
 export default class SessionUserService {
-    constructor(
-        private userRepository: IUserRepository,
-        private hashProvider: IHashProvider,
-    ) { }
+  constructor(
+    private userRepository: IUserRepository,
+    private hashProvider: IHashProvider,
+  ) {}
 
-    public async execute({ email, password }: IRequest): Promise<IResponse> {
-        const user = await this.userRepository.findByEmail(email);
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.userRepository.findByEmail(email);
 
-        if (!user) {
-            throw new AppError('Incorrect email/password combination.', 401);
-        }
-
-        const matchPassword = await this.hashProvider.compareHash(password, user.password);
-
-        if (!matchPassword) {
-            throw new AppError('Incorrect email/password combination.', 401);
-        }
-
-        const { secret, expireIn } = authConfig.jwt;
-
-        const token = sign({}, secret, {
-            subject: user.id,
-            expiresIn: expireIn,
-        });
-
-        return {
-            user,
-            token
-        };
+    if (!user) {
+      throw new AppError('Incorrect email/password combination.', 401);
     }
+
+    const matchPassword = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
+
+    if (!matchPassword) {
+      throw new AppError('Incorrect email/password combination.', 401);
+    }
+
+    const { secret, expireIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn: expireIn,
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
 }
