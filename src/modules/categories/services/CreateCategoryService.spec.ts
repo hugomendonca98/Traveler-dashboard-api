1c651c31e3efa836eabf5c16b87e92f00c875eb1
hugo <hugomendonca9@gmail.com>
2022-01-15 17:40:05 -0300

    Refactoring Test: CreateCategoryService

--- src/modules/categories/services/CreateCategoryService.spec.ts
+++ src/modules/categories/services/CreateCategoryService.spec.ts
@@ -1,21 +1,27 @@
 import AppError from '@shared/errors/appError';
+import FakeStorageProvider from '@shared/providers/StorageProvider/fakes/FakeStorageProvider';
 import FakeCategoryRepository from '../repositories/fakes/FakeCategoryRepository';
 import CreateCategoryService from './CreateCategoryService';
 
 let fakeCategoryRepository: FakeCategoryRepository;
+let fakeStorageProvider: FakeStorageProvider;
 let createCategoryService: CreateCategoryService;
 
 describe('CreateCategory', () => {
   beforeEach(() => {
     fakeCategoryRepository = new FakeCategoryRepository();
-    createCategoryService = new CreateCategoryService(fakeCategoryRepository);
+    fakeStorageProvider = new FakeStorageProvider();
+    createCategoryService = new CreateCategoryService(
+      fakeCategoryRepository,
+      fakeStorageProvider,
+    );
   });
 
   // Deve ser capaz de criar uma nova categoria.
   it('Should be able to create new category.', async () => {
     const category = await createCategoryService.execute({
       name: 'category name',
-      icon: 'icon name',
+      icon: 'iconFileName.jpg',
     });
 
     expect(category).toHaveProperty('id');
@@ -25,13 +31,13 @@ describe('CreateCategory', () => {
   it('Should not be able to create a new category with same name another.', async () => {
     await fakeCategoryRepository.create({
       name: 'category name',
-      icon: 'icon name',
+      icon: 'iconFileName.jpg',
     });
 
     await expect(
       createCategoryService.execute({
         name: 'category name',
-        icon: 'icon name',
+        icon: 'iconFileName.jpg',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
