dcfd585745850fc6783953f25588c18a7ad59889
hugomendonca98 <hugomendonca9@gmail.com>
2022-05-13 04:39:31 -0300

    fix icon image url

--- src/modules/categories/infra/typeorm/entities/Category.ts
+++ src/modules/categories/infra/typeorm/entities/Category.ts
@@ -1,3 +1,5 @@
+import upload from '@config/upload';
+import { Exclude, Expose } from 'class-transformer';
 import {
   Column,
   CreateDateColumn,
@@ -14,8 +16,25 @@ export default class Category {
   @Column()
   name: string;
 
+  @Exclude()
   @Column()
   icon: string;
+
+  @Expose({ name: 'icon_image' })
+  getImageUrl(): string | null {
+    if (!this.icon) {
+      return null;
+    }
+
+    switch (upload.driver) {
+      case 'disk':
+        return `${process.env.APP_API_URL}/files/${this.icon}`;
+      case 's3':
+        return `${process.env.APP_API_URL}/files/${this.icon}`;
+      default:
+        return null;
+    }
+  }
 
   @CreateDateColumn()
   created_at: Date;
