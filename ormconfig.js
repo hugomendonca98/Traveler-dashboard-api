1fda31bcec4a2a6deda1dcc87801e1c0a3142fe7
hugomendonca98 <hugomendonca9@gmail.com>
2022-07-22 00:10:13 -0300

    Heroku vars

--- ormconfig.js
+++ ormconfig.js
@@ -2,6 +2,10 @@ module.exports = [
   {
     name: "default",
     type: "postgres",
+    ssl: true,
+    extra: {
+      ssl: { rejectUnauthorized: false }
+    },
     url: process.env.DATABASE_URL,
     entities: [
       `./${process.env.ROOT_DIR}/modules/**/infra/typeorm/entities/*.{ts, js}`,
